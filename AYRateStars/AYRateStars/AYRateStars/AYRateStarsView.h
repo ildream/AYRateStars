daf0f86569880f4d0410c66b26766181d3899a7a
AnyaWork <858642232@qq.com>
2018-03-20 23:54:05 +0800

    逻辑判断属性提交

--- AYRateStars/AYRateStars/AYRateStars/AYRateStarsView.h
+++ AYRateStars/AYRateStars/AYRateStars/AYRateStarsView.h
@@ -10,4 +10,16 @@
 
 @interface AYRateStarsView : UIView
 
+//是否支持半星评级
+@property (nonatomic, assign) BOOL isSupportHalfStar;
+
+//是否支持第一颗星（或半颗星）选中后，无法消去选中状态
+@property (nonatomic, assign) BOOL isSupportKeepFirstStar;
+
+//是否支持滑动选中星星   default YES
+@property (nonatomic, assign) BOOL isSupportSwipeStar;
+
+//是否支持点击选中星星   default YES
+@property (nonatomic, assign) BOOL isSuppportTapStar;
+
 @end
