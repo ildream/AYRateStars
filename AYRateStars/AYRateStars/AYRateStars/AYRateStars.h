96dda6e15d1871215187185730717f6199c24a10
AnyaWork <858642232@qq.com>
2018-03-20 20:57:22 +0800

    提交工程

---
+++ AYRateStars/AYRateStars/AYRateStars/AYRateStars.h
@@ -0,0 +1,13 @@
+//
+//  AYRateStars.h
+//  AYRateStars
+//
+//  Created by Anya on 2018/3/20.
+//  Copyright © 2018年 Anya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AYRateStars : UIView
+
+@end
---
+++ AYRateStars/AYRateStars/AYRateStars/AYRateStars.m
@@ -0,0 +1,37 @@
+//
+//  AYRateStars.m
+//  AYRateStars
+//
+//  Created by Anya on 2018/3/20.
+//  Copyright © 2018年 Anya. All rights reserved.
+//
+
+#import "AYRateStars.h"
+
+@implementation AYRateStars
+
+- (id)initWithFrame:(CGRect)frame
+{
+    if (self = [super initWithFrame:frame])
+    {
+        
+    }
+    return self;
+}
+
+- (void)awakeFromNib
+{
+    [super awakeFromNib];
+    
+}
+
+- (id)initWithCoder:(NSCoder *)aDecoder
+{
+    if (self=[super initWithCoder:aDecoder])
+    {
+        
+    }
+    return self;
+}
+
+@end
